178ebd6edf33d9e770c4e4f8a8c434bb2bd52dd5
RichsonSimbabure <richson.simbabure@drimkoe.com>
2023-08-31 23:20:35 +0200

    chore: first challenge

--- twoSum.js
+++ twoSum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+//Excercise 1
 var twoSum = function(nums, target) {
     for(let a = 0; a < nums.length; a++){
         for(let b = a + 1; b < nums.length; b++){
